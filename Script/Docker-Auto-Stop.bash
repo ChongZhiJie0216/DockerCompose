97b8bd49fb27ce8b124838c9dccc5624c116c75b
ChongZhiJie <ChongZhiJie021604@gmail.com>
2023-04-11 11:37:20 +0800

    Add Script&Discord_Webhook_Template

--- Script/Docker-Auto-Stop.bash
+++ Script/Docker-Auto-Stop.bash
@@ -1,19 +1,85 @@
 #!/bin/bash
 #Discord_WebHook
-server_name=Unraid_Server
-user_name=Jiecloud_Unraid
-discord_webhook=https://discord.com/api/webhooks/1094420280773967973/7ORaBbESAA1_aGJdmwXbsbBhR6-kCWUtY2EXYGKavr6oCBRVxL8gbeAxW8l-bxVgNUit
+server_name=
+user_name=
+discord_webhook=
+avatar_url=
+server_icon_url=
 #Docker_Enviroment
-dir_path=/mnt/user/appdata
-dir=(Alist AllTube-Download-yt-dlp Discord-AutoDelete DiscordGSM DiscordRSS FastAPI-DLS FileBrowser FileBrowser-NovelAI 
-    Freenom Glances-Monitor Hexo Homarr IMMICH MariaDB Nextcloud NginxProxyManager OnlyOffice-DocumentServer OpenSpeedTest 
-    Portainer-CE Redis RSSHUB RustDesk Sudhanplayz-Discord-Musicbot-v5 UptimeKuma )
+dir_path=
+dir=()
 #Funtion
-curl -H "Content-Type: application/json" -d '{"username": "'$user_name'", "content": "''🖥️'"$server_name"' ''🕒:'"$(date +"%m-%d-%Y %T")"' 🔴All Docker Compose Stop⚠️  "}' "$discord_webhook"
+curl -H "Content-Type: application/json" -d'{
+  "username": "'$user_name'",
+  "avatar_url": "'$avatar_url'",
+  "content": "",
+  "embeds": [
+    {
+      "title": "Docker Compose Stoping Started",
+      "color": 16711680,
+      "description": "",
+      "timestamp": "",
+      "author": {
+        "name": "'$server_name'",
+        "icon_url": "'$server_icon_url'"
+      },
+      "image": {},
+      "thumbnail": {},
+      "footer": {
+        "text": "'"$(date +"%m-%d-%Y %T")"'"
+      },
+      "fields": []
+    }
+  ],
+  "components": []}' "$discord_webhook"
 for x in ${dir[@]}; do
-  curl -H "Content-Type: application/json" -d '{"username": "'$user_name'", "content": "''🖥️'"$server_name"' ''🕒:'"$(date +"%m-%d-%Y %T")"' 🟠Now Stopping : '"$x"' "}' "$discord_webhook"
+  curl -H "Content-Type: application/json" -d '{
+  "username": "'$user_name'",
+  "avatar_url": "'$avatar_url'",
+  "content": "",
+  "embeds": [
+    {
+      "title": "Docker Compose Stoping",
+      "color": 16690208,
+      "description": "Now Updating: '"$x"'",
+      "timestamp": "",
+      "author": {
+        "name": "'$server_name'",
+        "icon_url": "'$server_icon_url'"
+      },
+      "image": {},
+      "thumbnail": {},
+      "footer": {
+        "text": "'"$(date +"%m-%d-%Y %T")"'"
+      },
+      "fields": []
+    }
+  ],
+  "components": []}' "$discord_webhook"
   up_docker_dir=$dir_path/${x}
   cd "$up_docker_dir"
   docker compose down
 done
-curl -H "Content-Type: application/json" -d '{"username": "'$user_name'", "content": "''🖥️'"$server_name"' ''🕒:'"$(date +"%m-%d-%Y %T")"' 🟢All Docker Compose Stop Done✅  "}' "$discord_webhook"
+curl -H "Content-Type: application/json" -d'{
+  "username": "'$user_name'",
+  "avatar_url": "'$avatar_url'",
+  "content": "",
+  "embeds": [
+    {
+      "title": "All Docker Compose Stoped ",
+      "color": 1179392,
+      "description": "",
+      "timestamp": "",
+      "author": {
+        "name": "'$server_name'",
+        "icon_url": "'$server_icon_url'"
+      },
+      "image": {},
+      "thumbnail": {},
+      "footer": {
+        "text": "'"$(date +"%m-%d-%Y %T")"'"
+      },
+      "fields": []
+    }
+  ],
+  "components": []}' "$discord_webhook"
