29030d8b8f97aafefe887618140cfe3de2ca3eff
ChongZhiJie <ChongZhiJie021604@gmail.com>
2024-03-12 00:00:06 +0800

    Add & Update Script

--- Script/Docker-Auto-Start.bash
+++ Script/Docker-Auto-Start.bash
@@ -1,93 +1,60 @@
 #!/bin/bash
-sleep 1m
-docker network create -d macvlan --subnet=192.168.1.0/24 --gateway=192.168.1.1 -o parent=br0.10 vlan-net
-#Discord_WebHook
-server_name=
-user_name=
-discord_webhook=
-avatar_url=
-server_icon_url=
-#Docker_Enviroment
-dir_path=
-#Funtion
-if [ "$(cat $dir_path/foldername.txt)" = "$(find $dir_path -mindepth 1 -maxdepth 1 -type d -printf '%f\n')" ]; then
-  readarray -t dir < "$dir_path/foldername.txt"
+# Sleep for 30 seconds
+sleep 30s
+# Define Discord webhook variables
+server_name="Ubuntu"
+user_name="DockerBOT"
+discord_webhook="WEBHOOK_URL"
+avatar_url="https://www.docker.com/wp-content/uploads/2022/03/Moby-logo.png"
+server_icon_url="https://revdal.me/img/unraid.png"
+# Define Docker environment variables
+dir_path="/root/Docker-Compose/"
+
+# Create an array of folder names
+if [ ! -f "./foldername.txt" ]; then
+  find "./" -mindepth 1 -maxdepth 1 -type d -printf '%f\n' > "./foldername.txt"
 else
-  find $dir_path -mindepth 1 -maxdepth 1 -type d -printf '%f\n' > "$dir_path/foldername.txt"
-  readarray -t dir < "$dir_path/foldername.txt"
+  readarray -t dir < "./foldername.txt"
 fi
-curl -H "Content-Type: application/json" -d'{
-  "username": "'$user_name'",
-  "avatar_url": "'$avatar_url'",
-  "content": "",
-  "embeds": [
-    {
-      "title": "Docker Compose Starting",
-      "color": 16711680,
-      "description": "",
-      "timestamp": "",
-      "author": {
-        "name": "'$server_name'",
-        "icon_url": "'$server_icon_url'"
-      },
-      "image": {},
-      "thumbnail": {},
-      "footer": {
-        "text": "'"$(date +"%m-%d-%Y %T")"'"
-      },
-      "fields": []
-    }
-  ],
-  "components": []}' "$discord_webhook"
-for x in ${dir[@]}; do
+# Function to send Discord webhook message
+send_discord_message() {
+  local embed_title="$1"
+  local embed_description="$2"
+  local embed_color="$3"
   curl -H "Content-Type: application/json" -d '{
-  "username": "'$user_name'",
-  "avatar_url": "'$avatar_url'",
-  "content": "",
-  "embeds": [
-    {
-      "title": "Docker Compose Starting",
-      "color": 16690208,
-      "description": "Now Starting: '"$x"'",
-      "timestamp": "",
-      "author": {
-        "name": "'$server_name'",
-        "icon_url": "'$server_icon_url'"
-      },
-      "image": {},
-      "thumbnail": {},
-      "footer": {
-        "text": "'"$(date +"%m-%d-%Y %T")"'"
-      },
-      "fields": []
-    }
-  ],
-  "components": []}' "$discord_webhook"
+    "username": "'"$user_name"'",
+    "avatar_url": "'"$avatar_url"'",
+    "content": "",
+    "embeds": [
+      {
+        "title": "'"$embed_title"'",
+        "color": '"$embed_color"',
+        "description": "'"$embed_description"'",
+        "timestamp": "",
+        "author": {
+          "name": "'"$server_name"'",
+          "icon_url": "'"$server_icon_url"'"
+        },
+        "image": {},
+        "thumbnail": {},
+        "footer": {
+          "text": "'"$(date +"%m-%d-%Y %T")"'"
+        },
+        "fields": []
+      }
+    ],
+    "components": []
+  }' "$discord_webhook"
+}
+# Loop through the directory names
+for x in "${dir[@]}"; do
   up_docker_dir="$dir_path/${x}"
   cd "$up_docker_dir"
+  # Send Discord message before starting Docker Compose
+  send_discord_message "Docker Compose Starting" "Now Starting: '$x'" 16690208
+  # Start Docker Compose and sleep for 5 seconds
   docker compose up -d
-  sleep 10s
+  sleep 5s
 done
-curl -H "Content-Type: application/json" -d'{
-  "username": "'$user_name'",
-  "avatar_url": "'$avatar_url'",
-  "content": "",
-  "embeds": [
-    {
-      "title": "All Docker Compose  Started ",
-      "color": 1179392,
-      "description": "",
-      "timestamp": "",
-      "author": {
-        "name": "'$server_name'",
-        "icon_url": "'$server_icon_url'"
-      },
-      "image": {},
-      "thumbnail": {},
-      "footer": {
-        "text": "'"$(date +"%m-%d-%Y %T")"'"
-      },
-      "fields": []
-    }
-  ],
-  "components": []}' "$discord_webhook"
+# Send Discord message after all Docker Compose processes have started
+send_discord_message "All Docker Compose Started" "" 1179392
--- Script/Docker-Auto-Update.bash
+++ Script/Docker-Auto-Update.bash
@@ -1,94 +1,57 @@
 #!/bin/bash
-#Discord_WebHook
-server_name=
-user_name=
-discord_webhook=
-avatar_url=
-server_icon_url=
-#Docker_Enviroment
-dir_path=
-#Funtion
-if [ "$(cat $dir_path/foldername.txt)" = "$(find $dir_path -mindepth 1 -maxdepth 1 -type d -printf '%f\n')" ]; then
-  readarray -t dir < "$dir_path/foldername.txt"
-else
-  find $dir_path -mindepth 1 -maxdepth 1 -type d -printf '%f\n' > "$dir_path/foldername.txt"
-  readarray -t dir < "$dir_path/foldername.txt"
-fi
-curl -H "Content-Type: application/json" -d'{
-  "username": "'$user_name'",
-  "avatar_url": "'$avatar_url'",
-  "content": "",
-  "embeds": [
-    {
-      "title": "Docker Compose Updateing",
-      "color": 16711680,
-      "description": "",
-      "timestamp": "",
-      "author": {
-        "name": "'$server_name'",
-        "icon_url": "'$server_icon_url'"
-      },
-      "image": {},
-      "thumbnail": {},
-      "footer": {
-        "text": "'"$(date +"%m-%d-%Y %T")"'"
-      },
-      "fields": []
-    }
-  ],
-  "components": []}' "$discord_webhook"
-for x in ${dir[@]}; do
-  curl -H "Content-Type: application/json" -d '{
-  "username": "'$user_name'",
-  "avatar_url": "'$avatar_url'",
-  "content": "",
-  "embeds": [
-    {
-      "title": "Docker Compose Updateing",
-      "color": 16690208,
-      "description": "Now Updateing: '"$x"'",
-      "timestamp": "",
-      "author": {
-        "name": "'$server_name'",
-        "icon_url": "'$server_icon_url'"
-      },
-      "image": {},
-      "thumbnail": {},
-      "footer": {
-        "text": "'"$(date +"%m-%d-%Y %T")"'"
-      },
-      "fields": []
-    }
-  ],
-  "components": []}' "$discord_webhook"
-  up_docker_dir="$dir_path/${x}"
-  cd "$up_docker_dir"
-  docker compose pull
-  docker compose up -d
-  sleep 10s
-done
-echo "y" | docker image prune 
+# Define Discord webhook variables
+server_name="Ubuntu"
+user_name="DockerBOT"
+discord_webhook="WEBHOOK_URL"
+avatar_url="https://www.docker.com/wp-content/uploads/2022/03/Moby-logo.png"
+server_icon_url="https://revdal.me/img/unraid.png"
+# Define Docker environment variables
+dir_path="/root/Docker-Compose/"
+# Function to send a Discord message
+send_discord_message() {
+    local title="$1"
+    local description="$2"
+    local color="$3"
 
-curl -H "Content-Type: application/json" -d'{
-  "username": "'$user_name'",
-  "avatar_url": "'$avatar_url'",
-  "content": "",
-  "embeds": [
-    {
-      "title": "All Docker Compose Update Done ",
-      "color": 1179392,
-      "description": "",
-      "timestamp": "",
-      "author": {
-        "name": "'$server_name'",
-        "icon_url": "'$server_icon_url'"
-      },
-      "image": {},
-      "thumbnail": {},
-      "footer": {
-        "text": "'"$(date +"%m-%d-%Y %T")"'"
-      },
-      "fields": []
-    }
-  ],
-  "components": []}' "$discord_webhook"
+    curl -H "Content-Type: application/json" -d '{
+        "username": "'"$user_name"'",
+        "avatar_url": "'"$avatar_url"'",
+        "content": "",
+        "embeds": [
+            {
+                "title": "'"$title"'",
+                "color": '"$color"',
+                "description": "'"$description"'",
+                "author": {
+                    "name": "'"$server_name"'",
+                    "icon_url": "'"$server_icon_url"'"
+                },
+                "footer": {
+                    "text": "'"$(date +"%m-%d-%Y %T")"'"
+                }
+            }
+        ],
+        "components": []
+    }' "$discord_webhook"
+}
+# Check if foldername.txt exists, and create it if not
+if [ ! -f "./foldername.txt" ]; then
+    find "./" -mindepth 1 -maxdepth 1 -type d -printf '%f\n' > "./foldername.txt"
+else
+    readarray -t dir < <(tac "./foldername.txt")
+fi
+# Send initial Discord message
+send_discord_message "Docker Compose Start Update" "" 16711680
+# Loop through directories and update Docker Compose
+for x in "${dir[@]}"; do
+    send_discord_message "Docker Compose Updating" "Now Updating: '$x'" 16690208
+    up_docker_dir="$dir_path/${x}"
+    cd "$up_docker_dir"
+    docker compose pull
+    docker compose up -d
+    sleep 5s
+done
+# Prune Docker images
+echo "y" | docker image prune
+# Send final Discord message
+send_discord_message "Docker Compose Update Done" "" 1179392
